8adb850d93fe8d66d656b4f7e6ae15031015c464
Alexander <alexsvistunov73@gmail.com>
2017-01-20 15:37:40 +0300

    настроил табы

---
+++ js/my.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+	$('.tab-container .tab-content:not(:first)').hide();
+
+	$('.tabs li').click(function(event) {
+		event.preventDefault();
+		$('.tab-container .tab-content').hide();
+		$('.tabs .active').removeClass("active");
+		$(this).addClass('active');
+
+		var clicked = $(this).find('a:first').attr('href');
+		$('.tab-container ' + clicked).fadeIn('fast');
+	}).eq(0).addClass('active');
+	
+
+});
