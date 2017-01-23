8ce9602abeabdca8505b7fc4e3f84cb7cd5271c2
Alexander <alexsvistunov73@gmail.com>
2017-01-23 09:14:08 +0300

    дописал скрипт card-number (запрет ввода букв и др.символов кроме целых чисел)

--- js/card-number.js
+++ js/card-number.js
@@ -1,7 +1,19 @@
 var input = document.getElementById('card-number');
 
+//разделение числа на группы по 4цифры
+
     input.oninput = function() {
-    	n = input.value;
+    	var n = input.value;
     	if (n.length % 5 == 4 ) {n = n + " "}
-    	input.value = n;
-   };
+    	input.value = n; 
+
+ //запрет ввода символа, не являющегося целым числом или пробелом 	
+	    var str = input.value;    
+	    var length1 = str.length;
+
+	    if(str.search(/[^0-9\s]/i) != -1) {
+	    length1 = length1 - 1;
+	    str = str.substr(0, length1);
+	    input.value = str;
+	    }
+}
